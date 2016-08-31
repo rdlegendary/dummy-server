5a75380690890d58d2f72dd0472796fbb91995f8
Rick Dawson <rdawson@LEG-BUR-MKT-CSa.local>
2016-08-31 16:40:56 -0700

    Verified that Image Sends

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -2,39 +2,11 @@ var resemble   = require("node-resemble-js");
 
 module.exports.verify = function(req, res){
     var userImage = req.files.file;
-    
-    
-    var diff = resemble(userImage.path).onComplete(function(data){
-        console.log(data);
-    })
-    
-    /*
-            var diff = resemble(testFile).compareTo(newUserImage).ignoreAntialiasing().onComplete(function(data){
-            var matchPercentage = 100 - parseInt(data.misMatchPercentage);
-            res.json({match: matchPercentage});
-
-    
-
-    //Dummy Test.... For now
-   
-    
-    /*
-    
-    When OpenCV gets placed on the server.
-
-    cv.readImage(userFile, function(err, img){
-        console.log(img);
-        cv.readImage(testFile, function(err, img2){
-            console.log(img2);
-            cv.ImageSimilarity(img, img2, function(err, dis){
-                console.log("Images are off by " + dis);
-                var matchPercentage = 100 - dis;
-                res.json({match: matchPercentage});
-            })
-        })
-    });
-    */
-    
-   
+    var testFile  = global.absolutePath + '/images/test11.jpg';
+        
+            var diff = resemble(userImage.path).compareTo(testFile).ignoreAntialiasing().onComplete(function(data){
+                 var matchPercentage = 100 - parseInt(data.misMatchPercentage);
+                 res.json({match: matchPercentage});
+            });
     
 }
