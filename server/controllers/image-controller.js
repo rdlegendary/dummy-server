f8f8218224f6a9bd37ea4ba22a5b9595b0917b72
Rick Dawson <rdawson@LEG-BUR-MKT-CSa.local>
2016-08-31 17:52:03 -0700

    different validator

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -4,10 +4,10 @@ module.exports.verify = function(req, res){
     var userImage = req.files.file;
     var testFile  = global.absolutePath + '/images/test12.jpg';
         
-            var diff = resemble(userImage.path).compareTo(testFile).ignoreAntialiasing().onComplete(function(data){
+            var diff = resemble(userImage.path).compareTo(testFile).ignoreColors().onComplete(function(data){
                  var matchPercentage = 100 - parseInt(data.misMatchPercentage);
-                 console.log(matchPercentage);
-                 res.json({match: matchPercentage});
+                   console.log(matchPercentage);
+                   res.json({match: matchPercentage});
             });
     
 }
