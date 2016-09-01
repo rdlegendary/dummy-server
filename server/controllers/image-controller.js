323be6e07936e414706738854af3e9a66990c013
Rick Dawson <rdawson@LEG-BUR-MKT-CSa.local>
2016-09-01 09:50:58 -0700

    This is the test

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -4,7 +4,8 @@ module.exports.verify = function(req, res){
     var userImage = req.files.file;
     var testFile  = global.absolutePath + '/images/test12.jpg';
         
-            var diff = resemble(userImage.path).compareTo(testFile).ignoreColors().onComplete(function(data){
+            var diff = resemble(testFile).compareTo(userImage.path).onComplete(function(data){
+                console.log(data);
                  var matchPercentage = 100 - parseInt(data.misMatchPercentage);
                    console.log(matchPercentage);
                    res.json({match: matchPercentage});
