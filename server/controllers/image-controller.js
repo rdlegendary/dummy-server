080582dee121685ca5194e46d25974e6554930ab
Rick Dawson <rdawson@LEG-BUR-MKT-CSa.local>
2016-09-01 10:33:01 -0700

    adding 'ignoreAntialiasing' back for testing purposes

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -4,7 +4,7 @@ module.exports.verify = function(req, res){
     var userImage = req.files.file;
     var testFile  = global.absolutePath + '/images/test12.jpg';
         
-            var diff = resemble(testFile).compareTo(userImage.path).onComplete(function(data){
+            var diff = resemble(testFile).compareTo(userImage.path).ignoreAntialiasing().onComplete(function(data){
                 console.log(data);
                  var matchPercentage = 100 - parseInt(data.misMatchPercentage);
                    console.log(matchPercentage);
