1a9da4384181301205c566e557373325b9152084
Rick Dawson <rdawson@LEG-BUR-MKT-CSa.local>
2016-08-31 17:04:33 -0700

    new verification image

--- server/controllers/image-controller.js
+++ server/controllers/image-controller.js
@@ -2,7 +2,7 @@ var resemble   = require("node-resemble-js");
 
 module.exports.verify = function(req, res){
     var userImage = req.files.file;
-    var testFile  = global.absolutePath + '/images/test11.jpg';
+    var testFile  = global.absolutePath + '/images/test12.jpg';
         
             var diff = resemble(userImage.path).compareTo(testFile).ignoreAntialiasing().onComplete(function(data){
                  var matchPercentage = 100 - parseInt(data.misMatchPercentage);
